1cf45ee83b485b001c26702cb7680e50748069dd
manzoorsk <manzoor.kvana@gmail.com>
2017-03-17 11:48:13 +0530

    first commit

---
+++ app/src/main/java/com/example/admin/sqlitemain/Comment.java
@@ -0,0 +1,30 @@
+package com.example.admin.sqlitemain;
+
+/**
+ * Created by admin on 9/2/2016.
+ */
+public class Comment {
+    private long id;
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return comment;
+    }
+}
---
+++ app/src/main/java/com/example/admin/sqlitemain/DatabaseHelper.java
@@ -0,0 +1,43 @@
+package com.example.admin.sqlitemain;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by admin on 9/2/2016.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String TABLE_COMMENTS = "comments";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_COMMENT = "comment";
+
+    private static final String DATABASE_NAME = "commments.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_COMMENTS + "( " + COLUMN_ID
+            + " integer primary key autoincrement, " + COLUMN_COMMENT
+            + " text not null);";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null,DATABASE_VERSION );
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        Log.w(DatabaseHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS);
+        onCreate(db);
+
+    }
+}
